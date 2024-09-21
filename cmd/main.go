9f8742e39d32efb9779da1000b263293173af8a2
Tiago Cardoso <tiago.nascimento@dasa.com.br>
2024-09-21 13:11:26 -0300

    chore: format api response json

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/tiagoncardoso/fc/pge/multithreading/config"
 	"github.com/tiagoncardoso/fc/pge/multithreading/pkg/infra"
@@ -29,18 +30,19 @@ func main() {
 	url2 := buildApiUrl(conf.ApiRequest2URL, os.Args[1])
 
 	go makeRequest(url1, ch1, 0)
-	go makeRequest(url2, ch2, 1)
+	go makeRequest(url2, ch2, 0)
 
 	select {
 	case resp1 := <-ch1:
-		slog.Info("Faster Response from", "Api URL", url1, "resp:", resp1)
+		slog.Info(":", "Faster Response from", url1)
+		slog.Info("::", "Response Data:", formatResponse(resp1))
 	case resp2 := <-ch2:
-		slog.Info("Faster Response from", "Api URL", url2, "resp:", resp2)
+		slog.Info(":", "Faster Response from", url2)
+		slog.Info("::", "Response Data:", formatResponse(resp2))
 	case <-time.After(time.Second * time.Duration(conf.RequestTimeout)):
 		slog.Error("Timeout making request")
 	}
 
-	// TODO: format json response: Slog
 	// TODO: create documentation
 }
 
@@ -59,4 +61,13 @@ func makeRequest(url string, ch chan interface{}, delay int) {
 
 func buildApiUrl(originalUrl string, zipCode string) string {
 	return strings.Replace(originalUrl, "<<zip>>", zipCode, -1)
+}
+
+func formatResponse(resp interface{}) string {
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		slog.Error("Error formatting response", "msg", err)
+	}
+
+	return strings.Replace(string(jsonResp), "\"", "'", -1)
 }
