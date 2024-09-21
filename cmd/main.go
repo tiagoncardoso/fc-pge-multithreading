3f6385c8379ef50d832dfcc46949dc7b5d48a536
Tiago Cardoso <tiago.nascimento@dasa.com.br>
2024-09-21 12:03:48 -0300

    feat: add requester racing with fixed zip code

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"github.com/tiagoncardoso/fc/pge/multithreading/config"
+	"github.com/tiagoncardoso/fc/pge/multithreading/pkg/infra"
+	"log/slog"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	ch1 := make(chan interface{})
+	ch2 := make(chan interface{})
+
+	conf, err := config.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	go makeRequest(conf.ApiRequest1URL, ch1, 1)
+	go makeRequest(conf.ApiRequest2URL, ch2, 0)
+
+	select {
+	case resp1 := <-ch1:
+		slog.Info("Faster Response from", "Api URL", conf.ApiRequest1URL, "resp:", resp1)
+	case resp2 := <-ch2:
+		slog.Info("Faster Response from", "Api URL", conf.ApiRequest2URL, "resp:", resp2)
+	case <-time.After(time.Second * time.Duration(conf.RequestTimeout)):
+		slog.Error("Timeout making request")
+	}
+}
+
+func makeRequest(url string, ch chan<- interface{}, delay int) {
+	time.Sleep(time.Duration(delay) * time.Second)
+
+	requester := infra.NewApiRequester(url)
+	resp, err := requester.MakeRequest()
+	if err != nil {
+		slog.Error("Error making request", "msg", err)
+	}
+
+	ch <- resp
 }
---
+++ pkg/infra/api_requester.go
@@ -0,0 +1,45 @@
+package infra
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+type ApiRequester struct {
+	Url string
+}
+
+func NewApiRequester(url string) *ApiRequester {
+	return &ApiRequester{Url: url}
+}
+
+func (a *ApiRequester) MakeRequest() (interface{}, error) {
+	var apiResponse interface{}
+	cl := http.Client{}
+
+	req, err := http.NewRequest("GET", a.Url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := cl.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(body, &apiResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return apiResponse, nil
+}
